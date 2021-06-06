7f666816538a60b320238f0ea80d36ebde44fc03
VdustR <VdustR@gmail.com>
2021-06-06 23:09:25 +0800

    handle resize on startup

--- src/pages/conf2020/index.tsx
+++ src/pages/conf2020/index.tsx
@@ -25,7 +25,7 @@ const title = "回顧 2020";
 const description =
   "第一屆的年會，我們的主軸會希望能讓更多人知道何謂領域驅動設計(DDD)，以及讓更多人意識到其實 DDD 真的不是只是一票技術人員寫寫程式，關注程式碼本身結構性的問題而已，所以預計會有演講與工作坊並進的方式來進行。今年我們預計有機會邀請到來自 DDD_Eu 、中國等地的講者，並且同時結合台灣對於領域驅動設計長年有研究並且有意願與大家分享的老朋友們，一起與大家共同探討這個已經出生了10多年的老方法能給大家帶來的新體驗與感受。";
 
-const breakingWidth = 800;
+const breakingWidth = 640;
 const isServer = !ExecutionEnvironment.canUseDOM;
 const initWidth = isServer ? breakingWidth + 1 : window.innerWidth;
 
@@ -106,6 +106,7 @@ export default memo(() => {
       if (cancel || isServer) return;
       setWidth(window.innerWidth);
     };
+    handleResize();
     const throttleResize = throttle(handleResize, 300);
     window.addEventListener("resize", throttleResize);
     return () => {
